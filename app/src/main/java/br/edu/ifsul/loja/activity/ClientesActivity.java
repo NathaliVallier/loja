a6752ae01270a11fded849d6b012763d98dcfd0a
Nathali <nathalivallier@gmail.com>
2019-09-17 09:39:37 -0300

    update 17/09

--- app/src/main/java/br/edu/ifsul/loja/activity/ClientesActivity.java
+++ app/src/main/java/br/edu/ifsul/loja/activity/ClientesActivity.java
@@ -1,4 +1,61 @@
 package br.edu.ifsul.loja.activity;
 
-public class ClientesActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifsul.loja.R;
+import br.edu.ifsul.loja.adapter.ClientesAdapter;
+import br.edu.ifsul.loja.model.Cliente;
+
+public class ClientesActivity extends AppCompatActivity {
+
+    private static final String TAG = "ClientesActivity";
+    private ListView lvClientes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produtos);
+
+        //mapeia o componente da view
+        lvClientes = findViewById(R.id.lv_clientes);
+
+        //buscar os dados no RealTimeDataBase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("vendas/clientes");
+        myRef.orderByChild("nome").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "dataSnapshot=" + dataSnapshot);
+                List<Cliente> listCliente = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()){
+                    Log.d(TAG, "dataSnapshot=" + ds);
+                    Cliente cliente = ds.getValue(Cliente.class);
+                    cliente.setKey(ds.getKey());
+                    cliente.setIndex(listCliente.size());
+                    listCliente.add(cliente);
+                }
+
+                lvClientes.setAdapter(new ClientesAdapter(ClientesActivity.this, listCliente));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
 }
--- app/src/main/java/br/edu/ifsul/loja/adapter/ClientesAdapter.java
+++ app/src/main/java/br/edu/ifsul/loja/adapter/ClientesAdapter.java
@@ -1,4 +1,69 @@
 package br.edu.ifsul.loja.adapter;
 
-public class ClientesAdapter {
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.edu.ifsul.loja.R;
+import br.edu.ifsul.loja.model.Cliente;
+
+public class ClientesAdapter extends ArrayAdapter<Cliente> {
+    private final Context context;
+
+    public ClientesAdapter(@NonNull Context context, @NonNull List<Cliente> clientes) {
+        super(context, 0, clientes);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        final ClientesAdapter.ViewHolder holder;
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.activity_cliente, parent, false);
+            holder = new ClientesAdapter.ViewHolder(convertView);
+            convertView.setTag(holder);
+        }else{
+            holder = (ClientesAdapter.ViewHolder) convertView.getTag();
+        }
+
+        Cliente cliente = getItem(position);
+        holder.tvNome.setText(cliente.getNome());
+        holder.tvSobrenome.setText(cliente.getSobrenome());
+        holder.tvCpf.setText(cliente.getCpf().toString());
+        if(cliente.getUrl_foto().equals("")){
+            holder.pbFotoDoCliente.setVisibility(View.INVISIBLE);
+        }else{
+            //faz o downlod da foto aqui, usando API do serviço Storage
+        }
+
+        return convertView;
+    }
+
+    private class ViewHolder{
+        final TextView tvNome;
+        final TextView tvSobrenome;
+        final TextView tvCpf;
+        final ImageView imvFoto;
+        final ProgressBar pbFotoDoCliente;
+
+        public ViewHolder(View view){
+            tvNome = view.findViewById(R.id.tvNomeClienteAdapter);
+            tvSobrenome = view.findViewById(R.id.tvSobrenomeClienteAdapter);
+            tvCpf = view.findViewById(R.id.tvCpfClienteAdapater);
+            imvFoto = view.findViewById(R.id.imvFotoClienteAdapter);
+            pbFotoDoCliente = view.findViewById(R.id.pb_foto_clientes_adapter);
+        }
+    }
 }
